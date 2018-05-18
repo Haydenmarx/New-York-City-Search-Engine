52eff573d01d76124831370df62be4b89ea48f35
Haydenmarx <haydenmarx@gmail.com>
2018-05-18 11:44:17 -0700

    Set up Postgres db and flyway migrations

---
+++ flyway/sql/V1__create_users_and_feeds_and_join.sql
@@ -0,0 +1,16 @@
+create table USERS (
+  ID serial primary key,
+  USERNAME varchar NOT NULL
+);
+
+create table FEEDS (
+  ID serial primary key,
+  URL varchar NOT NULL,
+  PARAMS varchar ARRAY
+);
+
+create table CHAPTERS (
+  ID serial primary key,
+  USER_ID integer references USERS,
+  FEED_ID integer references FEEDS 
+);
