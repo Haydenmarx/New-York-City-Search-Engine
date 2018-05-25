707777a6b493c8a08c2d93beaba8153ffa298805
Haydenmarx <haydenmarx@gmail.com>
2018-05-25 10:48:52 -0700

    Styling for the jobs list

--- user-interface/src/app/components/job/job.component.ts
+++ user-interface/src/app/components/job/job.component.ts
@@ -7,19 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class JobComponent implements OnInit {
 
+  @Input() job: any;
   job_description = false;
-  minimmum_qual_requirements = false;
+  display = false;
+  minimum_qual_requirements = false;
   preferred_skills = false;
   hours_shift = false;
   to_apply = false;
 
   toggle = (category: string) => {
-    this[category] = true;
+    this[category] = !this[category];
   }
 
   constructor() { }
-
-  @Input() job: any;
 
   ngOnInit() {
   }
